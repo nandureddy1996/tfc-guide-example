4f178a225065e6018f2f41e2ca163cc4df871995
nandureddy1996 <87515685+nandureddy1996@users.noreply.github.com>
2024-04-30 20:29:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,7 +5,7 @@ provider "aws" {
 resource "aws_instance" "myec2" {
     ami = "ami-0cf2b4e024cdb6960"
     instance_type = "t2.micro"
-    
+    subnet_id = "subnet-0e8f623d0d536f5ae"
     tags = {
         Name = "myec2"
     }
